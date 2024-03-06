9205061a08049c352423664a792bf0618297d233
octavioaguila <oeaguila@uc.cl>
2024-03-06 20:01:34 -0300

    feat(angle added)

--- controllers/blue_robot/blue_robot.py
+++ controllers/blue_robot/blue_robot.py
@@ -2,6 +2,7 @@ from controller import Robot
 import numpy as np
 import cv2
 from PIL import Image
+import math
 
 TIMESTEP = 16
 robot = Robot()
@@ -55,13 +56,28 @@ while robot.step(TIMESTEP) != -1:
     # Apply the mask to the original BGR image
     img_masked = cv2.bitwise_and(img, img, mask=combined_mask)
 
+
     for bbox in bbox_detected:
         if bbox[1] is not None:
             x1, y1, x2, y2 = bbox[1]
             cv2.rectangle(img_masked, (x1, y1), (x2, y2), (0, 255, 0), 2)
             cv2.putText(img_masked, bbox[0], (x1, y1), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 1, cv2.LINE_AA)
 
-    
+    if bbox_detected[0][1] is not None and bbox_detected[1][1] is not None:
+        x1_yellow, y1_yellow, x2_yellow, y2_yellow = bbox_detected[0][1]
+        x1_blue, y1_blue, x2_blue, y2_blue = bbox_detected[1][1]
+
+        yellow_center_x = (x1_yellow + x2_yellow) / 2
+        blue_center_x = (x1_blue + x2_blue) / 2
+
+        yellow_center_y = (y1_yellow + y2_yellow) / 2
+        blue_center_y = (y1_blue + y2_blue) / 2
+
+        linear_distance = ((yellow_center_x - blue_center_x) ** 2 + (yellow_center_y - blue_center_y) ** 2) ** 0.5
+        angle = math.atan2(yellow_center_y - blue_center_y, yellow_center_x - blue_center_x) * 180 / math.pi
+        cv2.line(img_masked, (int(yellow_center_x), int(yellow_center_y)), (int(blue_center_x), int(blue_center_y)), (0, 255, 0), 2)
+        cv2.putText(img_masked, f"Linear Distance: {linear_distance}", (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
+        cv2.putText(img_masked, f"Angle: {angle}", (10, 100), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
 
     cv2.imshow('Blue Robot', img_masked)
     cv2.waitKey(1)
